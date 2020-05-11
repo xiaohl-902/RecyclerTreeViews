7ca0a67a7662432be20e4550de11886781ca5b12
胡礼 <xiaohl_902@163.com>
2020-05-11 16:15:01 +0800

    one commit

---
+++ app/src/main/java/tellh/com/recyclertreeview/MainActivity.java
@@ -0,0 +1,137 @@
+package tellh.com.recyclertreeview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import tellh.com.recyclertreeview.bean.Dir;
+import tellh.com.recyclertreeview.bean.File;
+import tellh.com.recyclertreeview.viewbinder.DirectoryNodeBinder;
+import tellh.com.recyclertreeview.viewbinder.FileNodeBinder;
+import tellh.com.recyclertreeview_lib.TreeNode;
+import tellh.com.recyclertreeview_lib.TreeViewAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rv;
+    private TreeViewAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initData();
+    }
+
+    private void initData() {
+        List<TreeNode> nodes = new ArrayList<>();
+        TreeNode<Dir> app = new TreeNode<>(new Dir("app"));
+        nodes.add(app);
+        app.addChild(
+                new TreeNode<>(new Dir("manifests"))
+                        .addChild(new TreeNode<>(new File("AndroidManifest.xml")))
+                        .addChild(new TreeNode<>(new Dir("recyclertreeview"))
+                                .addChild(new TreeNode<>(new Dir("Dir")))
+                                .addChild(new TreeNode<>(new Dir("DirectoryNodeBinder")))
+                                .addChild(new TreeNode<>(new Dir("File")))
+                                .addChild(new TreeNode<>(new Dir("FileNodeBinder")))
+                                .addChild(new TreeNode<>(new Dir("TreeViewBinder"))))
+                        .addChild(new TreeNode<>(new Dir("recyclertreeview2"))
+                                .addChild(new TreeNode<>(new Dir("Dir")))
+                                .addChild(new TreeNode<>(new Dir("DirectoryNodeBinder")))
+                                .addChild(new TreeNode<>(new Dir("File")))
+                                .addChild(new TreeNode<>(new Dir("FileNodeBinder")))
+                                .addChild(new TreeNode<>(new Dir("TreeViewBinder"))))
+        );
+        app.addChild(
+                new TreeNode<>(new Dir("java")).addChild(
+                        new TreeNode<>(new Dir("tellh")).addChild(
+                                new TreeNode<>(new Dir("com")).addChild(
+                                        new TreeNode<>(new Dir("recyclertreeview"))
+                                                .addChild(new TreeNode<>(new Dir("Dir")))
+                                                .addChild(new TreeNode<>(new Dir("DirectoryNodeBinder")))
+                                                .addChild(new TreeNode<>(new Dir("File")))
+                                                .addChild(new TreeNode<>(new Dir("FileNodeBinder")))
+                                                .addChild(new TreeNode<>(new Dir("TreeViewBinder")))
+                                )
+                        )
+                )
+        );
+
+        TreeNode<Dir> res = new TreeNode<>(new Dir("res"));
+        nodes.add(res);
+        res.addChild(
+                new TreeNode<>(new Dir("layout"))
+//                        .lock() // lock this TreeNode
+                        .addChild(new TreeNode<>(new Dir("activity_main.xml")))
+                        .addChild(new TreeNode<>(new Dir("item_dir.xml")))
+                        .addChild(new TreeNode<>(new Dir("item_file.xml")))
+        );
+        res.addChild(
+                new TreeNode<>(new Dir("mipmap"))
+//                        .addChild(new TreeNode<>(new File("ic_launcher.png")))
+        );
+
+
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new TreeViewAdapter(nodes, Arrays.asList(new FileNodeBinder(), new DirectoryNodeBinder()));
+        // whether collapse child nodes when their parent node was close.
+//        adapter.ifCollapseChildWhileCollapseParent(true);
+        adapter.setOnTreeNodeListener(new TreeViewAdapter.OnTreeNodeListener() {
+            @Override
+            public boolean onClick(TreeNode node, RecyclerView.ViewHolder holder) {
+                if (!node.isLeaf()) {
+                    //Update and toggle the node.
+                    onToggle(!node.isExpand(), holder);
+//                    if (!node.isExpand())
+//                        adapter.collapseBrotherNode(node);
+                }
+                return false;
+            }
+
+            @Override
+            public void onToggle(boolean isExpand, RecyclerView.ViewHolder holder) {
+                DirectoryNodeBinder.ViewHolder dirViewHolder = (DirectoryNodeBinder.ViewHolder) holder;
+                final ImageView ivArrow = dirViewHolder.getIvArrow();
+                int rotateDegree = isExpand ? 90 : -90;
+                ivArrow.animate().rotationBy(rotateDegree)
+                        .start();
+            }
+        });
+        rv.setAdapter(adapter);
+    }
+
+    private void initView() {
+        rv = (RecyclerView) findViewById(R.id.rv);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.id_action_close_all:
+                adapter.collapseAll();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ app/src/main/java/tellh/com/recyclertreeview/viewbinder/DirectoryNodeBinder.java
@@ -0,0 +1,133 @@
+package tellh.com.recyclertreeview.viewbinder;
+
+import android.os.Handler;
+import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import tellh.com.recyclertreeview.R;
+import tellh.com.recyclertreeview.bean.Dir;
+import tellh.com.recyclertreeview_lib.TreeNode;
+import tellh.com.recyclertreeview_lib.TreeViewAdapter;
+import tellh.com.recyclertreeview_lib.TreeViewBinder;
+
+/**
+ * Created by tlh on 2016/10/1 :)
+ */
+
+public class DirectoryNodeBinder extends TreeViewBinder<DirectoryNodeBinder.ViewHolder> {
+
+    private String TAG = "DirectoryNodeBinder";
+    private TreeViewAdapter mAdapters;
+    //    private TreeNode nodes;
+    private View itemView;
+
+
+    @Override
+    public ViewHolder provideViewHolder(View itemView, TreeViewAdapter mAdapter) {
+        this.mAdapters = mAdapter;
+        this.itemView = itemView;
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void bindView( ViewHolder holder,  int position,  TreeNode node,  TreeViewAdapter mTreeViewAdapter) {
+//        this.nodes = node;
+        holder.ivArrow.setRotation(0);
+        holder.ivArrow.setImageResource(R.drawable.ic_keyboard_arrow_right_black_18dp);
+        int rotateDegree = node.isExpand() ? 90 : 0;
+        holder.ivArrow.setRotation(rotateDegree);
+        Dir dirNode = (Dir) node.getContent();
+        holder.tvName.setText(dirNode.dirName);
+        if (node.isLeaf())
+            holder.ivArrow.setVisibility(View.INVISIBLE);
+        else holder.ivArrow.setVisibility(View.VISIBLE);
+
+
+        if (node.getIsChecked()) {
+            holder.isCheckes.setChecked(true);
+        } else {
+            holder.isCheckes.setChecked(false);
+        }
+
+//        holder.isCheckes.setTag(position);
+        holder.isCheckes.setOnClickListener(new MyOnClickListener().setDatas(holder, node, mAdapters, position));
+
+//        holder.isCheckes.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (holder.isCheckes.isChecked()) {
+//                    node.setChildAllCheckedYes();
+//                    Log.e(TAG, "bindView3: " + node.getIsChecked() + "    tag :" + position);
+//                    mTreeViewAdapter.notifyDataSetChanged();
+//                } else {
+//                    node.setChildAllCheckedNo();
+//                    Log.e(TAG, "bindView4: " + node.getIsChecked() + "    tag :" + position);
+//                    mTreeViewAdapter.notifyDataSetChanged();
+//                }
+//            }
+//        });
+    }
+
+    class MyOnClickListener implements View.OnClickListener {
+        private TreeNode nodes;
+        private TreeViewAdapter mAdapter;
+        private ViewHolder holder;
+        private int tag;
+
+        public MyOnClickListener setDatas(ViewHolder holder, TreeNode nodes, TreeViewAdapter mAdapter, int tag) {
+            this.holder = holder;
+            this.nodes = nodes;
+            this.mAdapter = mAdapter;
+            this.tag = tag;
+            return this;
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (holder.isCheckes.isChecked()) {
+                nodes.setChildAllCheckedYes();
+                Log.e(TAG, "bindView3: " + nodes.getIsChecked() + "    tag :" + tag);
+                mAdapter.notifyDataSetChanged();
+            } else {
+                nodes.setChildAllCheckedNo();
+                Log.e(TAG, "bindView4: " + nodes.getIsChecked() + "    tag :" + tag);
+                mAdapter.notifyDataSetChanged();
+            }
+        }
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.item_dir;
+    }
+
+    public static class ViewHolder extends TreeViewBinder.ViewHolder {
+        private ImageView ivArrow;
+        private TextView tvName;
+        private CheckBox isCheckes;
+
+        public ViewHolder(View rootView) {
+            super(rootView);
+            this.ivArrow = (ImageView) rootView.findViewById(R.id.iv_arrow);
+            this.tvName = (TextView) rootView.findViewById(R.id.tv_name);
+            this.isCheckes = (CheckBox) rootView.findViewById(R.id.is_checkes);
+        }
+
+        public ImageView getIvArrow() {
+            return ivArrow;
+        }
+
+        public TextView getTvName() {
+            return tvName;
+        }
+
+        public CheckBox getIsCheckes() {
+            return isCheckes;
+        }
+    }
+}
